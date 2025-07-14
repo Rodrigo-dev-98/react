4ca4e6030592e78ba47ab6b0263ad240887aedca
Rodrigo Costa <rodrigo42538@gmail.com>
2025-07-14 15:03:23 -0300

    meu primeiro comentário no projeto React

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
 import React from 'react';
-import Header from './Header';
-import Home from './Home';
-import Footer from './Footer';
-import Produtos from './Produtos';
 
 const App = () => {
-  const { pathname } = window.location;
-  let Component;
-  if (pathname === '/produtos') {
-    Component = Produtos;
-  } else {
-    Component = Home;
-  }
-
   return (
     <div>
-      <Header />
-      <Component />
-      <Footer />
     </div>
   )
 
