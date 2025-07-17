3998d1da69f5a04406996c946bfe2c7cfec78ce6
Rodrigo Costa <rodrigo42538@gmail.com>
2025-07-17 18:01:47 -0300

    Aula de useState

--- src/Produto.jsx
+++ src/Produto.jsx
@@ -1,47 +1,28 @@
-import React from 'react'
+import React from 'react';
 
-const Produto = ({product}) => {
+const Produto = ({ produto }) => {
+  const [dados, setDados] = React.useState(null);
+
+  React.useEffect(() => {
+    if (produto !== null) {
+      fetch(`https://ranekapi.origamid.dev/json/api/produto/${produto}`)
+        .then((response) => response.json())
+        .then((json) => setDados(json));
+    }
+  }, [produto]);
+
+  if (dados === null) return null;
   return (
-    <div className='produto'>
-      <style>
-        {`
-          .produto {
-            border: 1px solid #eee;
-            border-radius: 10px;
-            padding: 10px;
-            margin: 10px 0;
-            width: 450px;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-          }
-          img {
-            width: 100%;
-            border-radius: 10px;
-            margin-top: 30px;
-          }
-
-          h1 {
-            font-size: 1.5rem;
-            margin: 10px 0;
-          }
-
-          p {
-            font-size: 1.2rem;
-            margin: 10px 0;
-            text-align: center;
-          }
-        `}
-      </style>
-      <img src={product.fotos[0].src} alt={product.fotos[0].titulo} />
-      <h2>{product.nome}</h2>
-      <p>R$ {Number(product.preco).toFixed(2).replace('.', ',')}</p>
-      <p>{product.descricao}</p>
-
-      <button>Comprar</button>
+    <div>
+      <h1>{dados.nome}</h1>
+      <p>
+        {Number(dados.preco).toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        })}
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Produto
+export default Produto;
